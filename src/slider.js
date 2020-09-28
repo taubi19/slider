fb056e23cfce5e072da507280f5e28883c034517
Primoz Golob <primoz.golob@viberate.com>
2020-09-28 22:45:36 +0200

    add progress and placeholder circles
    * add svg elements for progress and gray placeholder
    * define circle properties constants

--- src/slider.js
+++ src/slider.js
@@ -13,7 +13,40 @@ export default class Slider {
   }
 
   createSlider() {
-    const svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+    const width = 200;
+    const height = 200;
+    const strokeWidth = 10;
+    const r = width / 2 - strokeWidth * 2;
+    const cx = width / 2;
+    const cy = height / 2;
+    const circumference = 2 * Math.PI * r;
+    const NAMESPACE = 'http://www.w3.org/2000/svg';
+    const svg = document.createElementNS(NAMESPACE, 'svg');
+
+    svg.setAttribute('width', width);
+    svg.setAttribute('height', height);
+
+    const placeholderCircle = document.createElementNS(NAMESPACE, 'circle');
+    placeholderCircle.setAttribute('cx', cx);
+    placeholderCircle.setAttribute('cy', cy);
+    placeholderCircle.setAttribute('r', r);
+    placeholderCircle.setAttribute('stroke-width', strokeWidth);
+    placeholderCircle.setAttribute('stroke-dasharray', '4 1');
+    placeholderCircle.style.fill = 'none';
+    placeholderCircle.style.stroke = '#e6e6e6';
+
+    const progressCircle = document.createElementNS(NAMESPACE, 'circle');
+    progressCircle.setAttribute('cx', cx);
+    progressCircle.setAttribute('cy', cy);
+    progressCircle.setAttribute('r', r);
+    progressCircle.setAttribute('stroke-width', strokeWidth);
+    progressCircle.setAttribute('stroke-dashoffset', 10);
+    progressCircle.style.fill = 'transparent';
+    progressCircle.style.stroke = '#ebebeb';
+
+    svg.appendChild(placeholderCircle);
+    svg.appendChild(progressCircle);
+
     this.container.appendChild(svg);
   }
 }
