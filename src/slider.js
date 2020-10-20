4649c76e4089ecef0e0e76ec13b86b701b62ced0
Primoz Golob <primoz.golob@viberate.com>
2020-10-20 22:22:03 +0200

    add stroke width to translate x

--- src/slider.js
+++ src/slider.js
@@ -1,7 +1,5 @@
 export default class Slider {
-  constructor({
-    container, color, max, min, step, radius,
-  }) {
+  constructor({ container, color, max, min, step, radius }) {
     this.container = container;
     this.color = color;
     this.max = max;
@@ -17,70 +15,70 @@ export default class Slider {
   createSlider() {
     const width = 200;
     const height = 200;
-    const strokeWidth = 10;
-    this.r = width / 2 - strokeWidth * 2;
+    this.strokeWidth = 10;
+    this.r = width / 2 - this.strokeWidth * 2;
     const cx = width / 2;
     const cy = height / 2;
     const circumference = 2 * Math.PI * this.r;
 
     const step = 10;
     const max = 100;
-    const min = 10;// default value?
-    const NAMESPACE = 'http://www.w3.org/2000/svg';
-    const svg = document.createElementNS(NAMESPACE, 'svg');
-    svg.classList.add('circle');
-    svg.setAttribute('width', width);
-    svg.setAttribute('height', height);
+    const min = 10; // default value?
+    const NAMESPACE = "http://www.w3.org/2000/svg";
+    const svg = document.createElementNS(NAMESPACE, "svg");
+    svg.classList.add("circle");
+    svg.setAttribute("width", width);
+    svg.setAttribute("height", height);
 
-    const placeholderCircle = document.createElementNS(NAMESPACE, 'circle');
-    placeholderCircle.setAttribute('cx', cx);
-    placeholderCircle.setAttribute('cy', cy);
-    placeholderCircle.setAttribute('r', this.r);
-    placeholderCircle.setAttribute('stroke-width', strokeWidth);
-    placeholderCircle.setAttribute('stroke-dasharray', '4 1');
-    placeholderCircle.style.fill = 'none';
-    placeholderCircle.style.stroke = '#e6e6e6';
+    const placeholderCircle = document.createElementNS(NAMESPACE, "circle");
+    placeholderCircle.setAttribute("cx", cx);
+    placeholderCircle.setAttribute("cy", cy);
+    placeholderCircle.setAttribute("r", this.r);
+    placeholderCircle.setAttribute("stroke-width", this.strokeWidth);
+    placeholderCircle.setAttribute("stroke-dasharray", "4 1");
+    placeholderCircle.style.fill = "none";
+    placeholderCircle.style.stroke = "#e6e6e6";
 
-    const progressCircle = document.createElementNS(NAMESPACE, 'circle');
-    progressCircle.setAttribute('cx', cx);
-    progressCircle.setAttribute('cy', cy);
-    progressCircle.setAttribute('r', this.r);
-    progressCircle.setAttribute('stroke-width', strokeWidth);
-    progressCircle.setAttribute('stroke-dashoffset', 10);
-    progressCircle.style.fill = 'transparent';
-    progressCircle.style.stroke = '#ebebeb';
+    const progressCircle = document.createElementNS(NAMESPACE, "circle");
+    progressCircle.setAttribute("cx", cx);
+    progressCircle.setAttribute("cy", cy);
+    progressCircle.setAttribute("r", this.r);
+    progressCircle.setAttribute("stroke-width", this.strokeWidth);
+    progressCircle.setAttribute("stroke-dashoffset", 10);
+    progressCircle.style.fill = "transparent";
+    progressCircle.style.stroke = "#ebebeb";
 
-    const input = document.createElement('input');
-    input.type = 'number';
+    const input = document.createElement("input");
+    input.type = "number";
     input.max = max;
     input.min = min;
     input.step = step;
 
-    const initialTranslateX = cx - strokeWidth;
+    const initialTranslateX = cx - this.strokeWidth;
     const initialTranslateY = 0;
-    const knob = document.createElement('div');
-    knob.classList.add('knob');
+    const knob = document.createElement("div");
+    knob.classList.add("knob");
     knob.style.transform = `translate(${initialTranslateX}px, ${initialTranslateY}px)`;
     this.knob = knob;
 
     svg.appendChild(placeholderCircle);
     svg.appendChild(progressCircle);
 
-    const innerContainer = document.createElement('div');
-    innerContainer.classList.add('innerContainer');
+    const innerContainer = document.createElement("div");
+    innerContainer.classList.add("innerContainer");
 
     innerContainer.appendChild(svg);
     innerContainer.appendChild(knob);
     innerContainer.appendChild(input);
     this.container.appendChild(innerContainer);
 
-    knob.addEventListener('mousedown', this.startMoveKnob.bind(this));
-    document.addEventListener('mousemove', this.moveKnob.bind(this));
+    knob.addEventListener("mousedown", this.startMoveKnob.bind(this));
+    document.addEventListener("mousemove", this.moveKnob.bind(this));
   }
 
   startMoveKnob(e) {
     this.mouseDown = true;
-    document.addEventListener('mouseup', this.endMoveKnob.bind(this));
+    document.addEventListener("mouseup", this.endMoveKnob.bind(this));
   }
 
   endMoveKnob() {
@@ -92,14 +90,15 @@ export default class Slider {
 
     const radAlpha = Math.atan2(
       e.pageY - 100, // 100 width/height
-      e.pageX - 100,
+      e.pageX - 100
     );
 
     const x = 80 + Math.cos(radAlpha) * this.r;
     const y = 80 + Math.sin(radAlpha) * this.r;
 
     this.point = { x, y };
-    console.log('POINT AFTER MOVE', this.point);
-    this.knob.style.transform = `translate(${this.point.x}px, ${this.point.y}px)`;
+    this.knob.style.transform = `translate(${
+      this.point.x + this.strokeWidth
+    }px, ${this.point.y}px)`;
   }
 }
