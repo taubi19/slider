ad894c44b31d222a9d9d1f1679991d342770459a
Primoz Golob <primoz.golob@viberate.com>
2020-11-06 07:50:07 +0100

    eslint wuote rule, extend eslint:recommended

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,14 +3,13 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: [
-    'airbnb-base',
-  ],
+  extends: ["airbnb-base", "eslint:recommended"],
   parserOptions: {
     ecmaVersion: 12,
-    sourceType: 'module',
+    sourceType: "module",
   },
   rules: {
-    'import/prefer-default-export': 'off',
+    "import/prefer-default-export": "off",
+    quotes: "double",
   },
 };
